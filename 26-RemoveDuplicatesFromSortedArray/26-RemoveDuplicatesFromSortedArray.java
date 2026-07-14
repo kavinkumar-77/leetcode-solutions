8e3b6e2b1cc08ea2b25997d4bcbfb2078c12a49b
Kavin Kumar P <kavinkumar.p2006@gmail.com>
2026-07-14 13:54:56 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,13 @@
+// Last updated: 7/14/2026, 1:54:54 PM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
