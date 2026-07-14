938d704eb7ba42e018e3df1e22314bd6cd5dade4
Kavin Kumar P <kavinkumar.p2006@gmail.com>
2026-07-14 13:54:31 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,9 @@
+# Last updated: 7/14/2026, 1:54:28 PM
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s)==sorted(t)
