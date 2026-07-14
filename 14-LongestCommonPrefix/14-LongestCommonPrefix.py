51ce2ce2881b05dfc02376087deff52e0372e48f
Kavin Kumar P <kavinkumar.p2006@gmail.com>
2026-07-14 13:55:00 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,12 @@
+# Last updated: 7/14/2026, 1:54:58 PM
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for word in strs[1:]:
+            while word.find(prefix)!=0:
+                prefix=prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
