8d3136f9e4cf7d27f0d0a35fb162d14f096c0d9a
Kavin Kumar P <kavinkumar.p2006@gmail.com>
2026-08-22 11:59:39 +0530

    Create 606-ConstructStringFromBinaryTree

---
+++ 606-ConstructStringFromBinaryTree/606-ConstructStringFromBinaryTree.py
@@ -0,0 +1,24 @@
+# Last updated: 8/22/2026, 11:59:37 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        result=[]
+        def preorder(node):
+            if not node:
+                return
+            result.append(str(node.val))
+            if node.left or node.right:
+                result.append("(")
+                preorder(node.left)
+                result.append(")")
+            if node.right:
+                result.append("(")
+                preorder(node.right)
+                result.append(")")
+        preorder(root)
+        return "".join(result)
