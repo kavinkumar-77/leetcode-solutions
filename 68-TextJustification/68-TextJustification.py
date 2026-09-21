b3b28ae6d646222773daa6dc647137da85ed1d32
Kavin Kumar P <kavinkumar.p2006@gmail.com>
2026-09-21 11:49:22 +0530

    Create 68-TextJustification

---
+++ 68-TextJustification/68-TextJustification.py
@@ -0,0 +1,13 @@
+# Last updated: 9/21/2026, 11:49:21 AM
+class Solution:
+    def fullJustify(self, words, maxWidth):
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
